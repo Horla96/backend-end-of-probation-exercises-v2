fdcb1a86328860c24895db6ce3d9ce82f1cfd858
Ridwan Sulaiman <152272649+Horla96@users.noreply.github.com>
2025-08-05 19:12:38 +0100

    Add files via upload
    Add Event Scheduler Project Files

---
+++ EventService.cs
@@ -0,0 +1,128 @@
+﻿using event_scheduler_and_conflict_detector_api.Dtos;
+using event_scheduler_and_conflict_detector_api.Enums;
+using event_scheduler_and_conflict_detector_api.Models;
+
+namespace event_scheduler_and_conflict_detector_api.Services
+{
+    public class EventService : IEventService
+    {
+        private static readonly Dictionary<Guid, Event> _events = new();
+
+        public EventService()
+        {
+            var event1 = new Event(new DateTime(2025, 06, 20, 12, 0, 0))
+            {
+                Id = Guid.NewGuid(),
+                Title = "Workers meeting",
+                Description = "Employess",
+                Location = "office",
+                Attendees = new List<string> { " OLA", "ATO" },
+                EventType = EventType.Meeting
+            };
+            var event2 = new Event(new DateTime(2025, 06, 20, 15, 0, 0))
+            {
+                Id = Guid.NewGuid(),
+                Title = "MD appointment",
+                Description = "Employment",
+                Location = "office",
+                Attendees = new List<string> { "Ridwan" },
+                EventType = EventType.Appointment
+            };
+            _events[event1.Id] = event1;
+            _events[event2.Id] = event2;
+        }
+
+        public bool HasConflict(Event newEvent)
+        {
+            return _events.Values.Any(e =>
+            e.Location.Equals(newEvent.Location, StringComparison.OrdinalIgnoreCase) &&
+            e.StartTime < newEvent.EndTime && newEvent.StartTime < e.EndTime);
+        }
+
+        public void AddEvent(CreateEventDto dto)
+        {
+            var newEvent = new Event(dto.StartTime)
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+                Location = dto.Location,
+                Attendees = dto.Attendees,
+                EventType = dto.EventType,
+                StartTime = dto.StartTime
+            };
+
+            if (HasConflict(newEvent))
+            {
+                throw new InvalidOperationException("Event clashes with an existing event at same time and location");
+            }
+            _events[newEvent.Id] = newEvent;
+        }
+
+        public bool DeleteEvent(Guid id)
+        {
+            return _events.Remove(id);
+        }
+
+        public IEnumerable<Event> GetAllEvent()
+        {
+            var events = _events.Values;
+            if (!events.Any())
+            {
+                throw new Exception("Events not found");
+            }
+            return events;
+        }
+
+        public Event GetEventById(Guid id)
+        {
+            if (!_events.TryGetValue(id, out var newEvent))
+            {
+                throw new KeyNotFoundException($"Event with ID: {id} not found");
+            }
+            return newEvent;
+        }
+
+        public IEnumerable<Event> GetEventsByDate(DateTime date)
+        {
+            return _events.Values
+                .Where(e => e.StartTime.Date == date.Date)
+                .ToList();
+
+        }
+
+        public IEnumerable<Event>GetEventsInRange(DateTime start, DateTime end)
+        {
+            if (start > end)
+                throw new ArgumentException("Start date be earlier thn end date");
+
+            return _events.Values
+                .Where(e => e.StartTime >= start && e.StartTime <= end)
+                .ToList();
+
+        }
+
+        public void UpdateEvent(Guid id, UpdateEventDto dto)
+        {
+            if (!_events.TryGetValue(id, out var events))
+            {
+                throw new KeyNotFoundException($"Event with ID: {id} not found");
+            }
+            var updatedEvent = new Event(dto.StartTime)
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+                Location = dto.Location,
+                Attendees = dto.Attendees,
+                EventType = dto.EventType,
+                StartTime = dto.StartTime
+            };
+
+
+            if (HasConflict(updatedEvent))
+            {
+                throw new InvalidOperationException("Event clashes with an existing event at same time and location");
+            }
+            _events[id] = updatedEvent;
+        }
+    }
+}
---
+++ EventsController.cs
@@ -0,0 +1,105 @@
+﻿using event_scheduler_and_conflict_detector_api.Dtos;
+using event_scheduler_and_conflict_detector_api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace event_scheduler_and_conflict_detector_api.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+
+        public EventsController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        [Microsoft.AspNetCore.Mvc.HttpGet("get all Events")]
+        public ActionResult GetAllEvent()
+        {
+            try
+            {
+                var events = _eventService.GetAllEvent();
+                return Ok(events);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet("id")]
+        public ActionResult GetEventById(Guid id)
+        {
+            try
+            {
+                var events = _eventService.GetEventById(id);
+                return Ok(events);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet("range")]
+        public ActionResult GetEventInRanges(DateTime start, DateTime end)
+        {
+            var events = _eventService.GetEventsInRange(start, end);
+
+            if (!events.Any())
+            {
+                return NotFound("No events found in the range");
+            }
+            return Ok(events);
+        }
+
+        [HttpPost("add an event")]
+        public IActionResult AddEvent(CreateEventDto dto)
+        {
+            try
+            {
+                _eventService.AddEvent(dto);
+                return Ok("Event Added");
+
+
+            }
+            catch(InvalidOperationException ex)
+            {
+                return Conflict(ex.Message);
+            }
+        }
+
+      [HttpPut("{id:guid}")]
+        public IActionResult UpdateEvent(Guid id, UpdateEventDto dto)
+        {
+            try
+            {
+                _eventService.UpdateEvent(id, dto);
+                return Ok("Event Updated Successfully");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Conflict(ex.Message);
+            }
+        }
+        [HttpDelete("{id:guid}")]
+        public ActionResult DeleteEvent(Guid id)
+        {
+            var deleted = _eventService.DeleteEvent(id);
+            if (!deleted)
+            {
+                return NotFound($"Event with ID: {id} not found");
+            }
+
+            return Ok("Event Deleted");
+        }
+    }
+}
